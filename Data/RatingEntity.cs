c83e23ddeb0230824d482ebdf206e92179ee3bda
Connor Fowler <connorfowlerroth@gmail.com>
2022-09-27 00:47:01 -0400

    RatingEntity, RestaurantDbContext, and RestaurantEntity added

---
+++ Data/RatingEntity.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantRaterMVC.Data
+{
+    public class RatingEntity
+    {
+        public int Id { get; set; }
+        public int RestaurantId { get; set; }
+        public double FoodScore { get; set; }
+        public double CleanlinessScore { get; set; }
+        public double AtmosphereScore { get; set; }
+
+        public virtual RestaurantEntity Restaurant { get; set; }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<RestaurantDbContext>(options => options.UseSqlServer(
+   builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
